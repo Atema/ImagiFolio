81ab2d0e3d6c99136ce637245793f1b26558fad9
Martijn Atema <martijn@atema.one>
2024-04-18 12:39:33 +0200

    Rename global prisma variable to avoid showing up

--- src/db/prisma/client.ts
+++ src/db/prisma/client.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "./generated";
 
 declare global {
-  var prisma: PrismaClient;
+  var __dev_prisma: PrismaClient;
 }
 
-const prisma = globalThis.prisma || new PrismaClient();
+const prisma = globalThis.__dev_prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.__dev_prisma = prisma;
 
 export default prisma;
