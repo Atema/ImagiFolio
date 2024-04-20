01398f1da3c8df6feb94b9ba173199fec303b362
Martijn Atema <martijn@atema.one>
2024-04-20 21:26:22 +0200

    Add user menu

--- src/components/nav-bar/UserMenu.tsx
+++ src/components/nav-bar/UserMenu.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { logoutUser } from "@/actions/auth";
+import cx from "@/utils/class-names/cx";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 export type UserMenuProps = {
   displayName: string;
@@ -8,9 +10,50 @@ export type UserMenuProps = {
 
 const UserMenu: React.FC<UserMenuProps> = ({ displayName }) => {
   return (
-    <form action={logoutUser}>
-      <button type="submit">{displayName}</button>
-    </form>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button className="focus:outline-0">{displayName}</button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content
+        arrowPadding={5}
+        sideOffset={5}
+        className={cx(
+          "min-w-40 border rounded-md mx-2 py-2 text-sm z-50",
+          "bg-white dark:bg-neutral-900 border-neutral-100 dark:border-neutral-800"
+        )}
+      >
+        <form>
+          <DropdownMenu.Arrow className="fill-neutral-100 dark:fill-neutral-800" />
+          {/* TODO: Re-activate when have settings page
+          <DropdownMenu.Item asChild>
+          <Link
+          href="/settings"
+          className={cx(
+                "block w-full text-start px-4 py-2 focus:outline-0",
+                "hover:bg-neutral-100 hover:dark:bg-neutral-800 focus:bg-neutral-100 focus:dark:bg-neutral-800"
+                )}
+                >
+                Settings
+                </Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator className="border-b my-2 border-neutral-100 dark:border-neutral-800" />
+          */}
+          <DropdownMenu.Item asChild onSelect={(e) => e.preventDefault()}>
+            <button
+              type="submit"
+              formAction={logoutUser}
+              className={cx(
+                "w-full text-start px-4 py-2 focus:outline-0",
+                "hover:bg-neutral-100 hover:dark:bg-neutral-800 focus:bg-neutral-100 focus:dark:bg-neutral-800"
+              )}
+            >
+              Sign out
+            </button>
+          </DropdownMenu.Item>
+        </form>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 };
 
