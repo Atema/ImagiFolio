97aaae79e4f5a928bcfcc7d5f140f6aa90da6266
Martijn Atema <martijn@atema.one>
2024-04-24 22:06:41 +0200

    Improve db seeding with album names

--- src/db/prisma/seed.ts
+++ src/db/prisma/seed.ts
@@ -1,52 +1,43 @@
 import { copyAndProcessPhoto } from "@/files/convert-photo";
 import { readdir } from "node:fs/promises";
-import { join } from "node:path";
-import { generate } from "random-words";
+import { hashPassword } from "../user";
 import prisma from "./client";
+import { join } from "node:path";
 
 const seedDir = process.env.SEED_IMAGE_DIR;
 if (!seedDir) process.exit();
-const seedImages = (await readdir(seedDir)).map((name) => join(seedDir, name));
 
-const user = await prisma.user.create({
+const admin = await prisma.user.create({
   data: {
-    email: "martijn@atema.one",
-    displayName: "Martijn Atema",
-    password: Buffer.from(
-      "002a8c129aa8e605bdd19bdbb2eb8cb0326f4c51c38545b900df4277df5a910211f8b5bc2c9c05623d1f980cdb11108796ed5d68e0a99d9ea2e5da5c43a08c1290ec89246ea953a9c0219b7cba8cdcae93",
-      "hex"
-    ),
+    email: "admin@example.com",
+    displayName: "Admin user",
+    password: await hashPassword("admin admin"),
   },
 });
 
-const albumNames = generate({
-  exactly: 10,
-  minLength: 4,
-  maxLength: 10,
-  wordsPerString: 3,
-  formatter: (word, idx) =>
-    idx === 0 ? word.slice(0, 1).toUpperCase().concat(word.slice(1)) : word,
-}) as string[];
+const seedAlbums = (await readdir(seedDir, { withFileTypes: true }))
+  .filter((entry) => entry.isDirectory)
+  .map((entry) => entry.name);
 
 const albums = await Promise.all(
-  albumNames.map((name) =>
+  seedAlbums.map((name) =>
     prisma.album.create({
       data: {
         name,
-        ownerId: user.id,
+        ownerId: admin.id,
       },
     })
   )
 );
 
 for (const album of albums) {
-  const albumSeedImages = seedImages
-    .map((image) => [image, Math.random()] as const)
-    .sort(([_a, a], [_b, b]) => a - b)
-    .map(([image]) => image)
-    .slice(0, Math.random() * Math.min(100, seedImages.length));
+  const seedImages = (
+    await readdir(join(seedDir, album.name), { withFileTypes: true })
+  )
+    .filter((entry) => entry.isFile)
+    .map((entry) => join(entry.path, entry.name));
 
   await Promise.all(
-    albumSeedImages.map((image) => copyAndProcessPhoto(album.id, image))
+    seedImages.map((image) => copyAndProcessPhoto(album.id, image))
   );
 }
