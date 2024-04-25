bf57223a7e26466b1b0e888b26688b78dfe60406
Martijn Atema <martijn@atema.one>
2024-04-25 21:57:58 +0200

    Move navbar component into layout page

--- src/app/(organising)/layout.tsx
+++ src/app/(organising)/layout.tsx
@@ -1,6 +1,7 @@
 import { getSession } from "@/actions/session";
-import NavBar from "@/components/nav-bar/NavBar";
+import Menu from "@/components/basic/Menu";
 import { getUser } from "@/db/user";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { FC, ReactNode } from "react";
 
@@ -14,15 +15,35 @@ const getCurrentUser = async () => {
   return user;
 };
 
-type OrganisingGroupLayoutProps = {
-  children: ReactNode;
-};
-
-const OrganisingGroupLayout: FC<OrganisingGroupLayoutProps> = async ({
+/**
+ * Layout for the organising group (overviews of photos and albums).
+ * Renders a navigation bar and containerises its children as the main layout
+ */
+const OrganisingGroupLayout: FC<{ children: ReactNode }> = async ({
   children,
 }) => (
   <>
-    <NavBar userName={(await getCurrentUser()).displayName} />
+    <div className="w-full py-4 border-b border-gray-dim mb-8">
+      <nav className="2xl:container 2xl:mx-auto px-2 md:px-4 flex flex-row">
+        <div className="flex-grow">
+          <Link href="/">ImagiFolio</Link>
+        </div>
+
+        <Menu
+          trigger={
+            <button className="focus:outline-0">
+              {(await getCurrentUser()).displayName}
+            </button>
+          }
+        >
+          {/* TODO: Re-add when have settings page
+        <Link href="/settings">Settings</Link>
+        ---
+        */}
+          <Link href="/logout">Sign out</Link>
+        </Menu>
+      </nav>
+    </div>
     <main className="2xl:container mx-auto px-2 md:px-4">{children}</main>
   </>
 );
